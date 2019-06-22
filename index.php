19f893499db5d50a296125afcc81c08871f354a2
PaolaSanchezCastillo <paolaescastillo@gmail.com>
2019-06-22 14:56:46 -0500

    Se añaden elementos de PHP POO y funciones

---
+++ index.php
@@ -0,0 +1,19 @@
+
+
+<?php include 'view/header.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+
+<div id="main">
+    <h1 class="top">Menu</h1>
+    <p><a href="product_manager">Product Manager</a></p>
+    <p><a href="product_catalog">Product Catalog</a></p>
+</div>
+</body>
+</html>
+<?php include 'view/footer.php'; ?>
